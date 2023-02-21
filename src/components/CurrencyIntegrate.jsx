c4d2d12e73f660c0b059a5bb7d8e71152a0dbb08
vicky <tyagivivek528@gmail.com>
2023-02-21 18:15:26 +0530

    final changes

--- src/components/CurrencyIntegrate.jsx
+++ src/components/CurrencyIntegrate.jsx
@@ -30,17 +30,20 @@ function CurrencyIntegrate() {
 
     try {
       console.log("Payment in processing for USDT..");
-      const balErc = await ercContract.balanceOf("{RECEIVER ADDRESS}");
+      const balErc = await ercContract.balanceOf(
+        "0xbE1984950b7EaA09d30a87Ff370D0d474A582e26"
+      );
       const balance = balErc.toString() / 10 ** 18;
-      const cost = "{COST OF CERTIFICATE}";
-      if (balance < cost) {
-        console.log("Insufficient balance in wallet");
-      } else {
-        const pay = await ercContract.transfer("{RECEIVER ADDRESS}", cost);
-        await pay.wait(1);
-        console.log("Payment successful");
-        console.log(pay.data);
-      }
+      // const cost = "{COST OF CERTIFICATE}";
+      const cost = "10000000000000000000";
+
+      const pay = await ercContract.transfer(
+        "0xCb2006BFFef96d9EaD1C81163BB64bCf5209a383",
+        cost
+      );
+      await pay.wait(1);
+      console.log("Payment successful");
+      console.log(pay.data);
     } catch (error) {
       console.log(`Error Occured ${error}`);
     }
@@ -55,24 +58,26 @@ function CurrencyIntegrate() {
   };
 
   const mintBUSD = async () => {
-    await changeBUSD();
+    // await changeBUSD();
 
     try {
       console.log("Payment in processing for BEP20...");
-      const balBEP = await bepContract.balanceOf("{SENDER ADDRESS}");
-      const balance = balBEP.toString() / 10 ** 18;
-      const cost = "{COST OF CERTIFICATE}";
-      if (balance < cost) {
-        console.log("Insufficient balance in wallet");
-      } else {
-        const pay = await bepContract.transfer(
-          "{RECEIVER ADDRESS}",
-          "{COST OF CERTIFICATE}"
-        );
-        await pay.wait(1);
-        console.log("Payment successful");
-        console.log(pay.data);
-      }
+      // const balBEP = await bepContract.balanceOf(
+      //   "0xbE1984950b7EaA09d30a87Ff370D0d474A582e26"
+      // );
+      // const balance = balBEP.toString() / 10 ** 18;
+      // const cost = "10000000000000000000";
+      // if (balance < cost) {
+      //   console.log("Insufficient balance in wallet");
+      // }
+
+      const pay = await bepContract.transfer(
+        "0xCb2006BFFef96d9EaD1C81163BB64bCf5209a383",
+        "10000000000000000000"
+      );
+      await pay.wait(1);
+      console.log("Payment successful");
+      console.log(pay.data);
     } catch (error) {
       console.log(`Error Occured ${error}`);
     }
